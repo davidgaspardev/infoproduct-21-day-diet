04d86560b1007e51e947a84daa54f5045a911a02
David Gaspar <davidgaspar.dev@gmail.com>
2018-09-21 23:30:37 -0300

    Added favicon

--- _js/text.js
+++ _js/text.js
@@ -1,19 +1,33 @@
 
+function insertTexts() {
 
-let text = new Array();
+	let text = new Array();
 
-text.push('" <strong>A Dieta de 21 dias é um método inovador e cientificamente comprovado, para você' + 
-	' perder de 5 a 10 quilos de gordura corporal em apenas 21 dias.</strong> " </br>' + 
-	' - Dr. Rodolfo Aurelio');
+	// <h1>
+	text.push('Top 10 dos infoprodutos mais vendidos no Hotmart');
 
-text.push('A Dieta de 21 dias é um revolucionário protocolo de emagrecimento (100% Natural) que foi ' + 
-	'baseado em mais de 1.200 experimentos científicos para ajudar você a perder peso e gordura Corporal. </br>' + 
-	' Mais rápido do que qualquer outro método que você já tentou.' + 
-	' Sem passar fome, sem contar calorias, sem abrir mão de alimentos deliciosos e sem ter que fazer exercícios monótonos e chatos.');
+	// <p>
+	text.push('" <strong>A Dieta de 21 dias é um método inovador e cientificamente comprovado, para você' +
+		' perder de 5 a 10 quilos de gordura corporal em apenas 21 dias.</strong> " </br>' +
+		' - Dr. Rodolfo Aurelio');
 
+	// <h1>
+	text.push('O que é a Dieta de 21 dias');
 
-let pText = document.getElementsByClassName('text');
+	// <p>
+	text.push('A Dieta de 21 dias é um revolucionário protocolo de emagrecimento (100% Natural) que foi ' +
+		'baseado em mais de 1.200 experimentos científicos para ajudar você a perder peso e gordura Corporal. </br>' +
+		' Mais rápido do que qualquer outro método que você já tentou.' +
+		' Sem passar fome, sem contar calorias, sem abrir mão de alimentos deliciosos e sem ter que fazer exercícios monótonos e chatos.');
 
-for(var i = 0; i < pText.length; i++) {
-	pText[i].innerHTML = text[i];
+	// Objects array of elements with class name "text"
+	let pText = document.getElementsByClassName('text');
+
+	
+	for(var i = 0; i < pText.length; i++) {
+		pText[i].innerHTML = text[i];
+	}
+
 }
+
+insertTexts();
